192b58c70e990aaa24771a5d039273cda46aec8c
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-05 10:44:31 +0500

    created a function that increments lapsCompleted and ran it 3 times

---
+++ 5-August-2026/lapFunctionIncrement.js
@@ -0,0 +1,9 @@
+let lapsCompleted = 0;
+// Create a function that increments lapsCompleted  and run it 3 times
+function incrementLapsCompleted() {
+  lapsCompleted = lapsCompleted + 1;
+}
+incrementLapsCompleted();
+incrementLapsCompleted();
+incrementLapsCompleted();
+console.log(lapsCompleted);
