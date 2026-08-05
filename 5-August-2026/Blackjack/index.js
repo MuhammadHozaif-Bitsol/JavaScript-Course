8998e99d4ec3b4df1a7cc56b81845afd4b59921f
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-05 17:53:30 +0500

    added new card functionality in the blackjack game

--- 5-August-2026/Blackjack/index.js
+++ 5-August-2026/Blackjack/index.js
@@ -1,12 +1,19 @@
-"use strict";
 let firstCard = 8;
 let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum");
+let cardsEl = document.getElementById("cards");
+console.log(sumEl);
 function startGame() {
+  renderGame();
+}
+function renderGame() {
+  sumEl.textContent = "SUM: " + sum;
+  cardsEl.textContent = "CARDS: " + firstCard + " " + secondCard;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -16,5 +23,10 @@ function startGame() {
     message = "You Lose";
     isAlive = false;
   }
-  console.log(message);
+  messageEl.textContent = message;
+}
+function newCard() {
+  let card = 3;
+  sum = sum + card;
+  renderGame();
 }
