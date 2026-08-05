636512085c542b4ac8af5ddcb0acbaa4e8f5bd40
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-05 15:23:08 +0500

    did if else statement practice

---
+++ 5-August-2026/Blackjack/index.js
@@ -0,0 +1,3 @@
+let firstCard = 3;
+let secondCard = 8;
+let sum = firstCard + secondCard;
---
+++ 5-August-2026/ifElse.js
@@ -0,0 +1,7 @@
+//check if person is old enough to enter the club
+let age = 22;
+if (age < 21) {
+  console.log("you can not enter");
+} else {
+  console.log("you can enter");
+}
