a1565b3ac70266144e45a08e10b9dc9520266562
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-05 11:02:15 +0500

    created a simple counter that increments the value of count by one everytime increment button is clicked

---
+++ 5-August-2026/Subway-Increment/index.js
@@ -0,0 +1,7 @@
+let count = 0;
+console.log(typeof count);
+//increment the value of count by one
+function increment() {
+  count = count + 1;
+  document.getElementById("count-el").innerText = count;
+}
