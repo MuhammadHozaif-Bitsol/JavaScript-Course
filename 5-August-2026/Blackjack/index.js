e605a7b0537914f043539e487a2afb7008558a66
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-05 16:37:45 +0500

    started working on the black jack game,completed simple cards logic and styling

--- 5-August-2026/Blackjack/index.js
+++ 5-August-2026/Blackjack/index.js
@@ -1,3 +1,20 @@
-let firstCard = 3;
-let secondCard = 8;
+"use strict";
+let firstCard = 8;
+let secondCard = 11;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "wohoo. You got a blackjack";
+    hasBlackJack = true;
+  } else {
+    message = "You Lose";
+    isAlive = false;
+  }
+  console.log(message);
+}
