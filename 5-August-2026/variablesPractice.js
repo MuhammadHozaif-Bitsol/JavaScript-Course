a7a0b18c777b68b3ab1ca23cd6d4edffe7dec427
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-05 14:12:37 +0500

    did string concatenation and made two methods,one adds 3 points the other removes one

---
+++ 5-August-2026/variablesPractice.js
@@ -0,0 +1,15 @@
+// concatentate first and last name
+let firstName = "Muhammad";
+let lastName = "Hozaif";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+// Create functions to add 3 points and one to remove one point
+let myPoints = 3;
+function addThreePoints() {
+  myPoints += 3;
+}
+function removeOnePoint() {
+  myPoints -= 1;
+}
+addThreePoints();
+console.log(myPoints);
