e428122cb0e25d92793abb5847e82d449871f77c
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-06 18:03:47 +0500

    added local storage to persist entries

--- 6-August-2026/ChromeExtension/index.js
+++ 6-August-2026/ChromeExtension/index.js
@@ -2,16 +2,23 @@ let myleads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const buttonEl = document.getElementById("input-btn");
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"));
+
 buttonEl.addEventListener("click", function () {
   myleads.push(inputEl.value);
+  localStorage.setItem("myleads", JSON.stringify(myleads));
   renderLeads();
   inputEl.value = "";
+  console.log(localStorage.getItem("myleads"));
 });
+if (leadsFromLocalStorage) {
+  myleads = leadsFromLocalStorage;
+  renderLeads();
+}
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myleads.length; i++) {
     listItems += `<li>  <a target=_blank href="${myleads[i]}"> ${myleads[i]}  </a> </li> `;
-    console.log(listItems);
   }
   ulEl.innerHTML = listItems;
 }
