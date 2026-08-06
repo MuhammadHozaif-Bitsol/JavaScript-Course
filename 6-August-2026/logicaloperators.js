3c46cf4b580648b56e9eb80acd754961aaa1e564
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-06 11:12:47 +0500

    practiced the and learnt about logical operators

---
+++ 6-August-2026/logicaloperators.js
@@ -0,0 +1,10 @@
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
+function showSolution() {
+  console.log("showing solution");
+}
+// Create an if statement that checks that both variables are false.
+// If so, run the showSolution() function
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+  showSolution();
+}
