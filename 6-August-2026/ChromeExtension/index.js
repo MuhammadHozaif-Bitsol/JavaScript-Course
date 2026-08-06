be198173e4e94aa370b9d69b8c8bc1663cd292d8
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-06 16:50:10 +0500

    used template literals in the chromeextension app

--- 6-August-2026/ChromeExtension/index.js
+++ 6-August-2026/ChromeExtension/index.js
@@ -1,15 +1,16 @@
-let myleads = ["awesomelead.com"];
+let myleads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const buttonEl = document.getElementById("input-btn");
 buttonEl.addEventListener("click", function () {
   myleads.push(inputEl.value);
   renderLeads();
+  inputEl.value = "";
 });
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myleads.length; i++) {
-    listItems += "<li>" + myleads[i] + "</li> ";
+    listItems += `<li>  <a target=_blank href="${myleads[i]}"> ${myleads[i]}  </a> </li> `;
     console.log(listItems);
   }
   ulEl.innerHTML = listItems;
