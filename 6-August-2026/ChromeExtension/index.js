12af352c2498aabd223d8622fa9a05c00e1e52d6
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-06 16:25:41 +0500

    in the leads tracking app ensured that a lead item gets render as soon as save input button is clicked

--- 6-August-2026/ChromeExtension/index.js
+++ 6-August-2026/ChromeExtension/index.js
@@ -1,4 +1,16 @@
-let buttonEl = document.getElementById("input-btn");
-buttonEl.addEventListener("onClick", function () {
-  console.log("button clicked");
+let myleads = ["awesomelead.com"];
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+const buttonEl = document.getElementById("input-btn");
+buttonEl.addEventListener("click", function () {
+  myleads.push(inputEl.value);
+  renderLeads();
 });
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myleads.length; i++) {
+    listItems += "<li>" + myleads[i] + "</li> ";
+    console.log(listItems);
+  }
+  ulEl.innerHTML = listItems;
+}
